ab0095333c9d7c4f1406583b75d8f6bd05ca8314
VictorTaelin <victortaelin@gmail.com>
2025-07-10 16:39:58 -0300

    fix stringification

--- src/Core/Type.hs
+++ src/Core/Type.hs
@@ -219,7 +219,7 @@ instance Show Term where
   show (Nil)          = "[]"
   show (Con h t)      = fromMaybe (show h ++ "<>" ++ show t) (prettyStr (Con h t))
   show (LstM x n c)   = "~ " ++ show x ++ " { []:" ++ show n ++ " ; <>:" ++ show c ++ " }"
-  show (Enu s)        = "&{" ++ intercalate "," (map (\x -> "&" ++ x) s) ++ "}"
+  show (Enu s)        = "enum{" ++ intercalate "," (map (\x -> "&" ++ x) s) ++ "}"
   show (Sym s)        = "&" ++ s
   show (EnuM x c e)   = "~ " ++ show x ++ " { " ++ intercalate " ; " (map (\(s,t) -> "&" ++ s ++ ": " ++ show t) c) ++ " ; " ++ show e ++ " }"
   show (Sig a b) = case b of
