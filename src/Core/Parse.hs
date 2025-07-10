b7315b6691f00dae6e78a5e0c2229d15d74b2876
VictorTaelin <victortaelin@gmail.com>
2025-07-10 09:42:18 -0300

    fixme/bug10

--- src/Core/Parse.hs
+++ src/Core/Parse.hs
@@ -111,7 +111,7 @@ isNameChar c = isAsciiLower c || isAsciiUpper c || isDigit c || c == '_' || c ==
 
 reserved :: [Name]
 -- The 'lambda' keyword is removed as part of the refactoring to expression-based matches.
-reserved = ["match","case","else","if","end","all","any","finally","import","as","and","or","def","log"]
+reserved = ["match","case","else","if","end","all","any","finally","import","as","and","or","def","log","gen"]
 
 -- | Parse a raw name without import resolution
 parseRawName :: Parser Name
