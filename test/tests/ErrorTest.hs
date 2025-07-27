8ac98f0e9f0edefa4d1c35946b7d92464bb168ad
VictorTaelin <victortaelin@gmail.com>
2025-07-27 10:34:47 -0300

    improve test

--- test/tests/ErrorTest.hs
+++ test/tests/ErrorTest.hs
@@ -10,4 +10,6 @@ def main : Nat = True
 main :: IO ()
 main = testFile main_bend
   "Should report a type mismatch error when assigning Bool to Nat." $ \out err -> do
-    assert ((err ++ out) `has` "Mismatch:")
+    assert (out `has` "Mismatch:")
+    assert (out `has` "Goal: Nat")
+    assert (out `has` "Type: Bool")
