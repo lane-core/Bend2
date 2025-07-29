b3d3df9745f35056c0145369051822e09fdf8356
Lorenzobattistela <lorenzobattistela@gmail.com>
2025-07-29 09:23:17 -0300

    bugfix scoping not respected

---
+++ test/tests/bugfix_scoping_not_respected_on_ctx.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE MultilineStrings #-}
+
+import Test
+-- scoping of variables when printing a context isn't respected.
+-- notice in the error that in A{(a)((a,()))==a}
+-- the first `a` should be (Σa:A. Unit) -> A, and the second, A.
+-- both `a` there are actually the same variable
+-- Suggestion: add depth: 
+--  - Goal: Σa:((Σa:A^0. Unit) -> A^0). ∀a:A^0. A^0{(a^0)((a^0,()))==a^0}
+
+scoping_not_respected_on_ctx :: String
+scoping_not_respected_on_ctx = """
+def exists(A: Set, P:A->Set) -> Set:
+  Σa:A.P(a)
+
+def thm(A:Set) -> exists((Σa:A.Unit)->A, λf.(∀a:A. A{f((a,())) == a})):
+  ()
+
+  # ✗ thm
+  # Mismatch:
+  # - Goal: Σa:((Σa:A. Unit) -> A). ∀a:A. A{(a)((a,()))==a}
+  # - Type: Unit
+  # Context:
+  # - A : Set
+  # Location: (line 16, column 3)
+  # 16 |   ()
+"""
+
+main :: IO ()
+main = testFile scoping_not_respected_on_ctx
+  "Scoping not respected when printing context" $ \out err -> do
+    assert (err `has` "Goal: Σa:((Σa:A^0. Unit) -> A^0). ∀a:A^0. A^0{(a^0)((a^0,()))==a^0}")
