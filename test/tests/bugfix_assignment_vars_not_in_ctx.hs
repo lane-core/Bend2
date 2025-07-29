a9b243831ff9acc7adafdcc248c068812d7437b9
Lorenzobattistela <lorenzobattistela@gmail.com>
2025-07-29 08:18:41 -0300

    bug15 test

---
+++ test/tests/bugfix_assignment_vars_not_in_ctx.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE MultilineStrings #-}
+
+import Test
+
+-- bug description:
+-- Variables coming from let assignments not showing in the context of the error.
+
+assignment_vars_not_in_ctx :: String
+assignment_vars_not_in_ctx = """
+def f() -> Unit:
+  (x,y) = (1n,2n)
+  z = 3n
+  True
+"""
+
+main :: IO ()
+main = testFile type_error_on_ctr_mismatch
+  "Variables coming from let assignments not showing in the context" $ \out err -> do
+    assert (err `has` "Mismatch:")
+    assert (err `has` "Goal: Unit")
+    assert (err `has` "Type: Bool")
+    assert (err `has` "x : Nat")
+    assert (err `has` "y : Nat")
+    assert (err `has` "z : Nat")
