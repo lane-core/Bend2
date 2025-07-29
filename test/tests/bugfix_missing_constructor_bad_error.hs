d8ad0645d7af02b3d042af521d327cd5628582d5
Lorenzobattistela <lorenzobattistela@gmail.com>
2025-07-29 12:39:54 -0300

    add test for missing constructor: bad error - not fixed

---
+++ test/tests/bugfix_missing_constructor_bad_error.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE MultilineStrings #-}
+
+import Test
+-- The following code results in a type mismatch error which is lacking a location,
+-- but the actual mistake is a missing argument to the @Wrap constructor in the case clause
+
+missing_constructor_bad_error :: String
+missing_constructor_bad_error = """
+type Wrapper:
+  case @Wrap: a :Nat
+
+def t(a:Wrapper) -> Nat:
+  match a:
+    case @Wrap: 0n
+"""
+
+-- ✓ Wrapper
+-- ✗ t
+-- Mismatch:
+-- - Goal: ∀y:(Σa:Nat. Unit). Nat
+-- - Type: Unit -> Set
+-- Context:
+-- Location: (line 0, column 0)
+
+main :: IO ()
+main = testFile missing_constructor_bad_error
+  "Missing constructor gives a mismatch, bad erroring." $ \out err -> do
+    assert (err `has` "missing constructor")
+    assert (not (err `has` "Mismatch"))
+
