0eae84692347e7f5c7d2a19001c42b03041bbd38
Lorenzobattistela <lorenzobattistela@gmail.com>
2025-07-29 10:03:52 -0300

    incorrect ann checks

---
+++ test/tests/bugfix_incorrect_ann_checks.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE MultilineStrings #-}
+
+-- Incorrect annotation does not stop it from type checking.
+import Test
+
+incorrect_ann_checks :: String
+incorrect_ann_checks = """
+def proj(p:Σa:Nat.Nat) -> Nat:
+  (m :: Bool) : Nat = ~p{(,):λx.λy.x}
+  m
+
+def main() -> Nat:
+  proj((2n,10n))
+"""
+
+main :: IO ()
+main = testFile incorrect_ann_checks
+  "incorrect annotation should not type check" $ \out err -> do
+    assert (err `has` "Mismatch:")
