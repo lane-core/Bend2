034a8e379b0bc3d74fd76d11e09eba4479d5d897
Paulo Cavalcanti <paulojcvf@gmail.com>
2025-07-29 20:24:34 +0400

    test: unsupproted match has no location or could be

---
+++ test/tests/bugfix_unsupported_match_no_location.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE MultilineStrings #-}
+
+import Test
+
+-- bug description:
+-- see https://github.com/HigherOrderCO/Bend2/issues/9
+--
+-- suggested to just add location, but perhaps ideally we should say "Can't assign to Bool"
+
+unsupported_match_no_location :: String
+unsupported_match_no_location = """
+def main() -> Nat:
+  Bool = 3n;
+  1n
+"""
+
+main :: IO ()
+main = testFile unsupported_match_no_location
+  "" $ \out err -> do
+    assert (err `has` "Unsupported pattern")
+    assert (err `has` "Location: (line ")
