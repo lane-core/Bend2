9b861757069ccde4b0c0a7ce5aed20a9ad4b422a
Lorenzobattistela <lorenzobattistela@gmail.com>
2025-07-29 10:00:36 -0300

    bugfix cant infer pair proj

---
+++ test/tests/bugfix_cant_infer_pair_proj_assign.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE MultilineStrings #-}
+
+import Test
+
+-- Should be inferrable, adding m : Nat annotation does not fix.
+-- # ✗ proj
+-- # CantInfer:
+-- # Context:
+-- # - p : Σa:Nat. Nat
+-- # Location: (line 8, column 7)
+-- # 8 |   m = ~p{(,):λx.λy.x}
+
+cant_infer_pair_proj_assign :: String
+cant_infer_pair_proj_assign = """
+def proj(p:Σa:Nat.Nat) -> Nat:
+  m = ~p{(,):λx.λy.x}
+  ()
+"""
+
+main :: IO ()
+main = testFileChecks cant_infer_pair_proj_assign
