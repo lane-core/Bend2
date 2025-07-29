88d5fb803a7b2f2d6cb1dc42f47d00bc298259f7
Lorenzobattistela <lorenzobattistela@gmail.com>
2025-07-29 12:43:58 -0300

    test: untyped assign value mismatch - not fixed

---
+++ test/tests/bugfix_untyped_assign_val_mismatch.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE MultilineStrings #-}
+
+import Test
+
+-- The following code gives a mismatch type error in the value
+-- of the variable ind at assignment, when no annotation means that
+-- this variable has no type that must be satisfied
+
+untyped_assign_val_mismatch :: String
+untyped_assign_val_mismatch = """
+type ListNat:
+  case @Nil:
+  case @Cons: h: Nat
+              t: ListNat
+def t(a:Nat, l:ListNat) -> Bool:
+  match l:
+    case @Nil: True
+    case @Cons{h,t}:
+      ind = t(h,t)
+      True
+"""
+
+-- ✓ ListNat
+-- ✗ t
+-- Mismatch:
+-- - Goal: _ -> _
+-- - Type: ListNat
+-- Context:
+-- - a : Nat
+-- - h : Nat
+-- - t : ListNat
+-- Location: (line 9, column 13)
+-- 9 |       ind = t(h,t)
+
+main :: IO ()
+main = testFileChecks untyped_assign_val_mismatch
